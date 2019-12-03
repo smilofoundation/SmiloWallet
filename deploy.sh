ab45f03c1c009657774c27b6e952e56980cde6be
Thomas Modeneis <thomas.modeneis@gmail.com>
2019-12-03 15:25:32 +0100

    Publishing to gh-pages (publish.sh)

--- deploy.sh
+++ deploy.sh
@@ -6,23 +6,27 @@
 #    exit 1;
 #fi
 
-echo "Deleting old publication"
-rm -rf publicweb
-mkdir publicweb
-git worktree prune
-rm -rf .git/worktrees/public/
-
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages publicweb origin/gh-pages
-
-echo "Removing existing files"
-rm -rf publicweb/*
-
+#echo "Deleting old publication"
+#rm -rf publicweb
+#mkdir publicweb
+#git worktree prune
+#rm -rf .git/worktrees/public/
+#
+#echo "Checking out gh-pages branch into public"
+#git worktree add -B gh-pages publicweb origin/gh-pages
+#
+#echo "Removing existing files"
+#rm -rf publicweb/*
+#
 
 mkdir -p publicweb
 
 echo "Generating site"
-npm run build:hash && cp -r dist/* publicweb
+#npm run build:hash && cp -r dist/* publicweb
+
+cp CNAME public/CNAME
+#cp 404.html public
+
 
 echo "Updating gh-pages branch"
 cd publicweb && git pull && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
